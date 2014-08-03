d5e32578ca7dd420fb55405c34bc042c4bc72081
Florian Stober <florian.stober@t-online.de>
2014-08-03 12:47:41 +0200

    removed unused import

--- src/codecrafter47/bungeetablistplus/bridge/BukkitBridge.java
+++ src/codecrafter47/bungeetablistplus/bridge/BukkitBridge.java
@@ -28,7 +28,6 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import net.md_5.bungee.api.ProxyServer;
