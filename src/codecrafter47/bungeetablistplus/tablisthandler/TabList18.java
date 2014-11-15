13dc3386111137af4732d57929168a81e73568e3
Florian Stober <Flo.e3@gmx.de>
2014-11-15 09:10:59 +0100

    fix bug

--- src/codecrafter47/bungeetablistplus/tablisthandler/TabList18.java
+++ src/codecrafter47/bungeetablistplus/tablisthandler/TabList18.java
@@ -241,7 +241,7 @@ public class TabList18 extends CustomTabList18 implements IMyTabListHandler {
         item.setProperties(new String[0][0]);
         pli.setItems(new Item[]{item});
         getPlayer().unsafe().sendPacket(pli);
-        send[row] = " ";
+        send[row] = null;
         slots_ping[row] = 0;
         sendTextures[row] = null;
     }
