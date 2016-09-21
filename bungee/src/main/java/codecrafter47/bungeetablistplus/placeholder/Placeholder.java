ffad1ddc8034a31288ef341031d340e8c569f356
Florian Stober <Flo.e3@gmx.de>
2016-09-21 09:56:14 +0200

    add `${viewer bungeecord_primary_group}` and `${player bungeecord_primary_group}`

--- bungee/src/main/java/codecrafter47/bungeetablistplus/placeholder/Placeholder.java
+++ bungee/src/main/java/codecrafter47/bungeetablistplus/placeholder/Placeholder.java
@@ -51,6 +51,7 @@ public abstract class Placeholder {
     static {
         playerPlaceholders.put("name", ofFunction(IPlayer::getName));
         playerPlaceholders.put("ping", ofFunction(player -> Integer.toString(player.getPing())));
+        playerPlaceholders.put("bungeecord_primary_group", ofStringData(DataKeys.BungeeCord_PrimaryGroup));
         playerPlaceholders.put("bungeeperms_display", ofStringData(DataKeys.BungeePerms_DisplayPrefix));
         playerPlaceholders.put("bungeeperms_prefix", ofStringData(DataKeys.BungeePerms_Prefix));
         playerPlaceholders.put("bungeeperms_suffix", ofStringData(DataKeys.BungeePerms_Suffix));
