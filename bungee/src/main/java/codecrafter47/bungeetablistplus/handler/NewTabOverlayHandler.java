CodeCrafter47/BungeeTabListPlus

commit adc6feafd8af02ba37f89bff5894e391a999e137

clear custom slots on server switch, should have the same effect as reloading the plugin; see #731

bungee/src/main/java/codecrafter47/bungeetablistplus/handler/NewTabOverlayHandler.java
@@ -651,7 +651,9 @@ public class NewTabOverlayHandler implements PacketHandler, TabOverlayHandler {
 
         @Override
         void onServerSwitch() {
-            // do nothing
+            if (player.getPendingConnection().getVersion() >= 764) {
+                clearCustomSlots();
+            }
         }
 
         @Override
@@ -688,6 +690,10 @@ public class NewTabOverlayHandler implements PacketHandler, TabOverlayHandler {
 
         @Override
         void onDeactivated() {
+            clearCustomSlots();
+        }
+
+        private void clearCustomSlots() {
             int customSlots = 0;
             for (int index = 0; index < 80; index++) {
                 if (slotState[index] != SlotState.UNUSED) {

commit 6226598fe7c66c178377abec065c4fabb9a7e886

fix issue introduced in adc6feaf

bungee/src/main/java/codecrafter47/bungeetablistplus/handler/NewTabOverlayHandler.java
@@ -701,6 +701,7 @@ public class NewTabOverlayHandler implements PacketHandler, TabOverlayHandler {
             for (int index = 0; index < 80; index++) {
                 if (slotState[index] != SlotState.UNUSED) {
                     customSlots++;
+                    dirtySlots.set(index);
                 }
             }
 